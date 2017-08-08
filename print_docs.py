71c80df72d80d0cc2d25700bd6218e90c89dcf9c
Zestyr <zestyr@protonmail.com>
2017-08-08 16:55:58 +0200

    more

--- print_docs.py
+++ print_docs.py
@@ -10,8 +10,9 @@ for api_name, fn in LBRYDaemon.callable_methods.iteritems():
     returns_no_type = re.search(r'Returns:\n +\([A-z]+\)', fn.__doc__)
     returns_section = re.search(r'Returns:\n( +\([A-z]+\) (?:\n?.+)*)\n(?: +[^A-z])?', fn.__doc__)
     if returns_section is not None:
-        print(returns_section.group(1))
-        continue
+        if "{" in returns_section.group(1):
+            print(returns_section.group(1))
+            break
         # if m.group(0) is not "Returns:\n":
         #     print("test")
         # print(m.group(0))
@@ -19,12 +20,10 @@ for api_name, fn in LBRYDaemon.callable_methods.iteritems():
         continue
         if returns_no_type is None:
             print("{} docstring must specify a return value".format(api_name))
-            continue
-        if returns is None:
+        elif returns is None:
             print("{} docstring has no section matching 'Returns:'".format(api_name))
-            continue
-
-        print("{} docstring couldn't be parsed.".format(api_name))
+        else:
+            print("{} docstring couldn't be parsed.".format(api_name))
 
 
 def call(command):
